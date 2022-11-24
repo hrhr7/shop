71377ece21e1acb2ad1ddf421ac1aa44ff7a9407
hrin <rrrin3255@gmail.com>
2022-11-24 12:23:45 +0900

    파라미터

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -57,4 +57,15 @@ public class ThymeleafExController {
         model.addAttribute("itemDtoList",itemDtoList);
         return "thymeleafEx/thymeleafEx04";
     }
+//    링크로 페이지 이동
+    @GetMapping("/ex05")
+    public String thymeleafExample05(){
+        return "thymeleafEx/thymeleafEx05";
+    }
+    @GetMapping("/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2",param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
 }
